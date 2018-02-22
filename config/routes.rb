b8831182a089d00a5e22650f0ae5ae5447e09bd1
Marta Jopp <martapeterson@gmail.com>
2018-02-22 10:01:12 -0600

    dynamic data

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,6 @@ Rails.application.routes.draw do
   # this says go to about controller and go to this method in the controller
   get 'about' => 'about#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'aboutshow' => 'about#show'
+
 end
