2442ffc185766b6140ffd4822aacd156d193168e
Marta Jopp <martapeterson@gmail.com>
2018-02-24 13:20:30 -0600

    added new model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180223142255) do
+ActiveRecord::Schema.define(version: 20180224191741) do
 
   create_table "people", force: :cascade do |t|
     t.string "firstname"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 20180223142255) do
   create_table "posts", force: :cascade do |t|
     t.string "posttext"
     t.string "postauthor"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
